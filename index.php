f1b1a2ae83b3933df9f8b839d1fecf0163cd8e11
marino5710 <brianmarin4@gmail.com>
2023-07-18 09:47:59 -0600

    se realizaron cambios para que funcionara

--- index.php
+++ index.php
@@ -8,23 +8,23 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <style>
-#table {
+/* #table {
     background-color: #E6F0FF;
-}
+} */
 </style>
 <body>
 <div class="container mt-5 bg-light">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h1 class="text-center display-6 fw-bold text-primary bg-warning p-3">Personajes de la serie de Ricky Mortin</h1>
-                <form method="POST" class="mb-4">
+                <form id="formMorty" method="POST" class="mb-4">
                     <div class="input-group">
                         <input type="text" name="morty" id="morty" class="form-control" placeholder="Ingrese el nombre del Personaje">
                         <button type="submit" class="btn btn-primary ms-2">Consultar</button>
                     </div>
                 </form>
                 <p id="estado" class="text-center bg-info"></p>
-                <table id="table" class="table table-bordered table-hover">
+                <table id="tablaMorty" class="table table-bordered table-hover">
                     <thead>
                         <tr>
                             <th>DATO</th>
@@ -37,11 +37,11 @@
                             <td id="nombreMorty"></td>
                         </tr>
                         <tr>
-                            <td>PESO</td>
+                            <td>Estado</td>
                             <td id="pesoMorty"></td>
                         </tr>
                         <tr>
-                            <td>TIPO</td>
+                            <td>Genero</td>
                             <td id="tipoMorty"></td>
                         </tr>
                         <tr>
@@ -57,50 +57,91 @@
             </div>
         </div>
     </div>
+    <!--  -->
     <!-- aqui empieza el html del segundo endpoint -->
+    <!--  -->
     <div class="container mt-5 bg-light">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <h1 class="text-center display-6 fw-bold text-primary bg-warning p-3">Personajes de la serie de Ricky Mortin</h1>
-                <form method="POST" class="mb-4">
-                    <div class="input-group">
-                        <input type="text" name="episodio" id="episodio" class="form-control" placeholder="Ingrese el nombre del Personaje">
-                        <button type="submit" class="btn btn-primary ms-2">Consultar</button>
-                    </div>
-                </form>
-                <p id="estadoEpisodio" class="text-center bg-info"></p>
-                <table id="table" class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>DATO</th>
-                            <th>VALOR</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>NOMBRE</td>
-                            <td id="nombreMorty"></td>
-                        </tr>
-                        <tr>
-                            <td>PESO</td>
-                            <td id="pesoMorty"></td>
-                        </tr>
-                        <tr>
-                            <td>TIPO</td>
-                            <td id="tipoMorty"></td>
-                        </tr>
-                        <tr>
-                            <td>CREADO</td>
-                            <td id="creadoMorty"></td>
-                        </tr>
-                        <tr>
-                            <td>IMAGEN</td>
-                            <td><img src="" alt="" id="imagenMorty" class="img-fluid"></td>
-                        </tr>
-                    </tbody>
-                </table>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <h1 class="text-center display-6 fw-bold text-primary bg-warning p-3">Episodios de la serie de Ricky Mortin</h1>
+        <form id="formEpisodio"method="POST" class="mb-4">
+            <div class="input-group">
+            <input type="text" name="episodio" id="episodio" class="form-control" placeholder="Ingrese el ID del episodio">
+            <button type="submit" class="btn btn-primary ms-2">Consultar</button>
             </div>
+        </form>
+        <p id="estadoEpisodio" class="text-center bg-info"></p>
+        <table id="tablaEpisodio" class="table table-bordered table-hover" style="display: none;">
+            <thead>
+            <tr>
+                <th>DATO</th>
+                <th>VALOR</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>ID</td>
+                <td id="episodioId"></td>
+            </tr>
+            <tr>
+                <td>Nombre del episodio</td>
+                <td id="episodioName"></td>
+            </tr>
+            <tr>
+                <td>Episodio al aire</td>
+                <td id="episodioAire"></td>
+            </tr>
+            <tr>
+                <td>CREADO</td>
+                <td id="episodioCreado"></td>
+            </tr>
+            </tbody>
+        </table>
         </div>
+    </div>
+    </div>
+
+    <!-- aqui empieza mi tercer endpoint busqueda de location por id  -->
+
+    <div class="container mt-5 bg-light">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <h1 class="text-center display-6 fw-bold text-primary bg-warning p-3">Episodios de la serie de Ricky Mortin</h1>
+        <form id="formEpisodio"method="POST" class="mb-4">
+            <div class="input-group">
+            <input type="text" name="episodio" id="episodio" class="form-control" placeholder="Ingrese el ID del episodio">
+            <button type="submit" class="btn btn-primary ms-2">Consultar</button>
+            </div>
+        </form>
+        <p id="estadoEpisodio" class="text-center bg-info"></p>
+        <table id="tablaEpisodio" class="table table-bordered table-hover" style="display: none;">
+            <thead>
+            <tr>
+                <th>DATO</th>
+                <th>VALOR</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>ID</td>
+                <td id="episodioId"></td>
+            </tr>
+            <tr>
+                <td>Nombre del episodio</td>
+                <td id="episodioName"></td>
+            </tr>
+            <tr>
+                <td>Episodio al aire</td>
+                <td id="episodioAire"></td>
+            </tr>
+            <tr>
+                <td>CREADO</td>
+                <td id="episodioCreado"></td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
     </div>
 
     <script src="script.js"></script>
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //constantes para el primer endpoint encontrar personaje por nombre 
-const formulario = document.querySelector('form');
-const tabla = document.querySelector('table');
-const button = document.querySelector('button[type="submit"]');
+const formulario = document.querySelector('#formMorty');
+const tabla = document.querySelector('#tablaMorty');
+const button = document.querySelector('#formMorty button[type="submit"]');
 
 ////constantes para el segundo endpoint buscar episodios por su nombre
 
-const formulario1 = document.querySelector('form');
-const tabla1 = document.querySelector('table');
-const button1 = document.querySelector('button[type="submit"]');
+const formulario1 = document.querySelector('#formEpisodio');
+const tabla1 = document.querySelector('#tablaEpisodio');
+const button1 = document.querySelector('#formEpisodio button[type="submit"]');
 
 const consultarMorty = async (e) => {
     e.preventDefault();
@@ -54,46 +54,47 @@ const consultarMorty = async (e) => {
 
     ///segundo endpoint
 
+//buscar personajes por genero y ordenarlos de mayor a menor seg
 
-    const consultarEpisodio = async (e) => {
-        e.preventDefault();
-        let nombreEpisodio = formulario1.episodio.value;
-        if (nombreEpisodio == '') {
-          alert("Debe ingresar el nombre del episodio");
-          return;
-        }
-        const url = `https://rickandmortyapi.com/api/character/?name=${nombreEpisodio}&status=alive`;
-        const config = {
-          method: 'GET'
-        };
-      
-        document.getElementById('estadoEpisodio').innerText = 'Buscando personaje...';
-        try {
-            const respuesta = await fetch(url, config);
-            if (respuesta.status === 200) {
-              const data = await respuesta.json();
-              console.log(data);
-              console.log(data.results[0].name);
-              console.log(data.results[0].status);
-              console.log(data.results[0].gender);
-              console.log(data.results[0].created);
-              console.log(data.results[0].image);
-        
-              document.getElementById('nombreMorty').innerText = data.results[0].name;
-              document.getElementById('pesoMorty').innerText = data.results[0].status;
-              document.getElementById('tipoMorty').innerText = data.results[0].gender;
-              document.getElementById('creadoMorty').innerText = data.results[0].created;
-              document.getElementById('imagenMorty').src = data.results[0].image;
-              document.getElementById('estado').innerText = 'Personaje encontrado';
-              tabla.style.display = '';
-            } else {
-              document.getElementById('estado').innerText = 'Personaje no encontrado';
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        };
-    
-        formulario.addEventListener('submit', consultarMorty);
-    
-    
+
+
+const consultarEpisodio = async (e) => {
+    e.preventDefault();
+    let idEpisodio = formulario1.episodio.value;
+    if (idEpisodio == '') {
+      alert("Debe ingresar el ID del episodio");
+      return;
+    }
+    const url = `https://rickandmortyapi.com/api/episode/${idEpisodio}`;
+  
+    const config = {
+      method: 'GET'
+    };
+  
+    document.getElementById('estadoEpisodio').innerText = 'Buscando episodio...';
+    try {
+      const respuesta = await fetch(url, config);
+      if (respuesta.status === 200) {
+        const data = await respuesta.json();
+        console.log(data);
+        console.log(data.id);
+        console.log(data.name);
+        console.log(data.air_date);
+        console.log(data.episode);
+        console.log(data.created);
+  
+        document.getElementById('episodioId').innerText = data.id;
+        document.getElementById('episodioName').innerText = data.name;
+        document.getElementById('episodioAire').innerText = data.air_date;
+        document.getElementById('episodioCreado').innerText = data.created;
+        document.getElementById('estadoEpisodio').innerText = 'Episodio encontrado';
+        tabla1.style.display = '';
+      } else {
+        document.getElementById('estadoEpisodio').innerText = 'Episodio no encontrado';
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  formulario1.addEventListener('submit', consultarEpisodio);
