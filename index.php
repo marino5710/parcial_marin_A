9852ce737275d9fa8cea07d10d32106ee2f7c86e
marino5710 <brianmarin4@gmail.com>
2023-07-18 08:06:14 -0600

    se crearon los divs

--- index.php
+++ index.php
@@ -8,3 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1 class="text-center">API Fetch</h1>
