7626fe324670f4e41c602a2dee417ea0a81b503d
marino5710 <brianmarin4@gmail.com>
2023-07-18 08:00:46 -0600

    se copio el html de la tarea 5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tarea 5</title>
+    <link rel="icon" type="image/x-icon" href="data:image/x-icon;,">
+</head>
+<body>
+    <h1>api fetch</h1>
+    <form method="POST">
+        <input type="text" name="pokemon" id="pokemon">
+        <button type="submit">Consultar</button> 
+        <button id="buttonConsulta" type="button">Consultar a PHP</button>
+    </form>
