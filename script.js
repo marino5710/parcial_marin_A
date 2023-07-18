a6270b0fe9a5253804cd7e86b71ca23221759490
marino5710 <brianmarin4@gmail.com>
2023-07-18 08:13:55 -0600

    const consultar morty

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const formulario = document.querySelector('form');
 const tabla = document.querySelector('table');
 const button = document.getElementById('buttonConsulta');
+
+
+const consultarMorty = async (e) => {
+    e.preventDefault();
+    let nombreMorty = formulario.morty.value;
+    if (nombreMorty == '') {
+      alert("Debe ingresar el nombre del personaje");
+      return;
+    }
+    
